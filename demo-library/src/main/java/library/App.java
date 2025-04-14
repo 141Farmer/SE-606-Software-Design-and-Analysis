055e7a5d21ccdcb2e1f185faf4b461ac8c9c0143
Md. Kamrul Hasan <bsse1418@iit.du.ac.bd>
2025-04-15 01:33:07 +0600

    wont work

---
+++ demo-library/src/main/java/library/App.java
@@ -0,0 +1,14 @@
+//package library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from another library" );
+    }
+}
+
